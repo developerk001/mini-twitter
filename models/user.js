dbbb69effb239cd31a84a9b8c9bd173bd67f515e
designerSohail <designersohail786@gmail.com>
2018-02-28 16:37:32 +0545

    generate avatar and password hashing methods added for user schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const crypto = require('crypto')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
@@ -26,4 +28,26 @@ const UserSchema = new Schema({
     }
   }]
 })
+
+UserSchema.pre('save', next => {
+  if (!user.isModified('password')) return next()
+  if (usre.password) {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) return next()
+      bcrypt.hash(this.password, salt, (err, hash) => {
+        if (err) return next()
+        this.password = hash
+        next()
+      })
+    })
+  }
+})
+
+UserSchema.methods.avatar = size => {
+  if (size) size = 200
+  if (!this.email) return `https://gravatar.com/avatar/?s=${size}&d=retro`
+  var md5 = crypto.cryptoHash('md5').update(this.email).digest('hex')
+  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`
+}
+
 module.exports = mongoose.model('User', UserSchema)
